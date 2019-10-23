3a25578c733de4fd57545136b2cf28e6c905e4e1
Love Palm (Paxec AB) <par.chalmers@gmail.com>
2019-10-23 14:59:48 +0200

    Put the trainloop in a separate file.

---
+++ DDQN/DDQN_trainLoop.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 14:30:43 2019
+
+@author: root
+"""
+
+import torch
+import numpy as np
+from numpy import array
+from collections import namedtuple
+from DDQN.DDQNhelpers import *
+
+
+def train_loop_ddqn( env, ddqn, replay_buffer, num_episodes, enable_visualization=False, batch_size=64, gamma=.94):        
+    """ A loop that can be used to train a DDQN network. Will usually be somewhat application dependant,
+        but this is a baseline that could/should work for the trailer-vehicle environment. """
+    
+    Transition = namedtuple("Transition", ["s", "a", "r", "next_s", "t"])
+    #Things we log per each time step each episode. 
+    perStepLogTuple = namedtuple("perStepLog", ["Px", "Py","angleRad", "action"])
+    
+    #Nesteld tuples 
+    perEpisodeLogTuple = namedtuple("perEpLog", ["perStepLog", "eps", "R", "Ravg"])
+    
+    trainingLog = perEpisodeLogTuple(perStepLog = [], eps = [], R=[], Ravg=[] )
+    
+    #Initial and final probability of taking a random action
+    eps = 0.99
+    eps_end = 0.1 
+    
+    #Epsilon decays this much each apisode
+    #eps_decay = .01
+    eps_decay = 0.002
+    tau = 1000
+    cnt_updates = 0
+    R_buffer = []
+    R_avg = []
+    
+    print('Starting to train.')
+    
+    for i in range(num_episodes):
+        state = env.reset() # Initial state
+        state = state[None,:] # Add singleton dimension, to represent as batch of size 1.
+        finish_episode = False # Initialize
+        ep_reward = 0 # Initialize "Episodic reward", i.e. the total reward for episode, when disregarding discount factor.
+        q_buffer = []
+        steps = 0
+        maxSteps = 100
+        
+        perStepLog = perStepLogTuple(Px =[], Py =[], action =[], angleRad = [])
+        
+        while not finish_episode and steps < maxSteps:
+           
+            if enable_visualization:
+                env.render() # comment this line out if you don't want to / cannot render the environment on your system
+            steps += 1
+
+            # Take one step in environment. No need to compute gradients,
+            # we will just store transition to replay buffer, and later sample a whole batch
+            # from the replay buffer to actually take a gradient step.
+            q_online_curr, curr_action = calc_q_and_take_action(ddqn, state, eps)
+            q_buffer.append(q_online_curr)
+            
+            #Velocity is constant for now
+            new_state, reward, finish_episode,  = env.step(curr_action) # take one step in the evironment
+            
+            perStepLog.Px.append(new_state[0] )
+            perStepLog.Py.append(new_state[1] )
+            perStepLog.angleRad.append(new_state[2] )
+            perStepLog.action.append(curr_action)
+            
+            #set_trace()
+            
+            new_state = new_state[None,:]
+            
+            
+            # Assess whether terminal state was reached.
+            # The episode may end due to having reached 200 steps, but we should not regard this as reaching the terminal state, and hence not disregard Q(s',a) from the Q target.
+            # https://arxiv.org/abs/1712.00378
+            #nonterminal_to_buffer = not finish_episode or steps == maxSteps
+            
+            #For this agent the above shoudlnt hold
+            nonterminal_to_buffer = not finish_episode 
+            
+            # Store experienced transition to replay buffer
+            replay_buffer.add(Transition(s=state, a=curr_action, r=reward, next_s=new_state, t=nonterminal_to_buffer))
+
+            state = new_state
+            ep_reward += reward
+            
+            # If replay buffer contains more than 1000 samples, perform one training step
+            if replay_buffer.buffer_length > 1000:
+                
+                loss = sample_batch_and_calculate_loss(ddqn, replay_buffer, batch_size, gamma)
+                ddqn.optimizer.zero_grad()
+                loss.backward()
+                ddqn.optimizer.step()
+
+                cnt_updates += 1
+                if cnt_updates % tau == 0:
+                    ddqn.update_target_network()
+        #########################
+        ## End of episode
+        eps = max(eps - eps_decay, eps_end) # decrease epsilon        
+        
+        R_buffer.append(ep_reward)
+         
+        # Running average of episodic rewards (total reward, disregarding discount factor)
+        R_avg.append(.05 * R_buffer[i] + .95 * R_avg[i-1])  if i > 0 else  R_avg.append(R_buffer[i])
+        
+        trainingLog.perStepLog.append(perStepLog)
+        trainingLog.eps.append(eps)
+        trainingLog.Ravg.append(R_avg[-1])
+        trainingLog.R.append(ep_reward )
+
+        if(i%10 == 0):
+            print('Episode: {:d}, Total Reward (running avg): {:4.0f}, Epsilon: {:.3f}'.format( i, R_avg[-1], eps))
+        #print('Episode: {:d}, Total Reward (running avg): {:4.0f} ({:.2f}) Epsilon: {:.3f}, Avg Q: {:.4g}'.format(i, ep_reward, R_avg[-1], eps, np.mean(np.array(q_buffer))))
+        
+        # If running average > 195 (close to 200), the task is considered solved
+        if R_avg[-1] > 195:
+            return trainingLog
+        
+    print("Training finished.")
+    return trainingLog
--- train_DDQN.py
+++ train_DDQN.py
@@ -13,10 +13,54 @@ import gym
 from collections import namedtuple
 import DDQN
 from DDQN import DDQNhelpers
-
+from DDQN.DDQN_trainLoop import train_loop_ddqn
 from DDQN.dqn_model import DoubleQLearningModel, ExperienceReplay
 import DDQN.dqn_model
 from DDQN.DDQNhelpers import *
 
 from IPython.core.debugger import set_trace
 from gymEnvironments.bicycleEnv import *
+
+# Enable visualization? Does not work in all environments.
+enable_visualization = False
+
+#Actions are full turn left, straight, full turn right
+#actions = (-1,0,1)
+
+
+##System initial conditions. 
+L = 2 #Length rear axis to front axis
+Ts = 0.2 #Sample interval in seconds. 
+
+#Position x, y, and heading
+initState = (5,0, 0)
+
+#Yes, the truck is a bicycle these days. 
+truck = BicycleEnv(L,Ts, initState)
+
+device = torch.device("cpu")
+
+
+# Initializations
+
+num_actions = len(truck.action_map.keys()) #TODO: Hardcoded now, do something more fancy later.
+num_states = len(truck.initState)
+
+#Training hyperparameters. 
+num_episodes = 700
+batch_size = 128
+gamma = .94
+learning_rate = 1e-3
+
+# Object holding our online / offline Q-Networks
+ddqn = DoubleQLearningModel(device, num_states, num_actions, learning_rate)
+
+# Create replay buffer, where experience in form of tuples <s,a,r,s',t>, gathered from the environment is stored 
+# for training
+replay_buffer = ExperienceReplay(device, num_states)
+
+# Train
+trainingLog = train_loop_ddqn( truck, ddqn, replay_buffer, num_episodes, enable_visualization, batch_size, gamma)
+
+
+
