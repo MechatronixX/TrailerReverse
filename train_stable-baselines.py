63191306ff350049a51c6a8724fdef1ced40c97f
Love Palm (Paxec AB) <par.chalmers@gmail.com>
2019-10-24 19:12:40 +0200

    Hyperparameter tuning.

--- train_stable-baselines.py
+++ train_stable-baselines.py
@@ -9,7 +9,7 @@ Created on Thu Oct 24 14:07:54 2019
 #Try to the train a system using stable baselines. Example from 
 #https://stable-baselines.readthedocs.io/en/master/modules/ppo2.html
 import gym
-
+import numpy as np
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO2
@@ -31,7 +31,7 @@ def train():
     env = SubprocVecEnv([lambda: CarTrailerParkingRevEnv()  for i in range(n_cpu)])
 
     model = PPO2(MlpPolicy, env, verbose=1)
-    model.learn(total_timesteps=500000, log_interval=10, callback = callback)
+    model.learn(total_timesteps=np.int(1e6), log_interval=10, callback = callback)
     model.save("ppo2_trailer")
 
     #del model # remove to demonstrate saving and loading
