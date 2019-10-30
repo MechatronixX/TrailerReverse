8252349900ead5e39102e84b17f64c1f5e948898
Love Palm (Paxec AB) <par.chalmers@gmail.com>
2019-10-30 16:07:10 +0100

    Trying to log training data. It seems that the basic idea is to wrap the environment is something called Monitor(), which logs to harddrive.

--- train_PPO_stable-baselines.py
+++ train_PPO_stable-baselines.py
@@ -29,20 +29,83 @@ import numpy as np
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO2
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+
+
+import matplotlib as plt
+
+import numpy as np
+import matplotlib.pyplot as pltimport 
+import os
 
 from gymEnvironments.gym_reverse_variable_trailer_number import Reverse_variable_trailer_number_environment
 
-def callback(_locals, _globals):    
+log_dir = "/tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+
+def callback(_locals, _globals): 
+    #print(_locals, type(_locals))
+    
+    #So the callback returns locals() and globals() from the learn() function in
+    #https://github.com/hill-a/stable-baselines/blob/master/stable_baselines/ppo2/ppo2.py
+    #About locals/globals here: https://thepythonguru.com/python-builtin-functions/locals/
+    #print("Hallu", _locals['timesteps'])
+    #_locals['self'].save(log_dir + 'best_model.pkl')
     return True 
+
 
 def train():
     # multiprocess environment
+   
     n_cpu = 4
     env = SubprocVecEnv([lambda: Reverse_variable_trailer_number_environment() for i in range(n_cpu)])
-
-    model = PPO2(MlpPolicy, env, verbose=1, learning_rate=1e-5)
-    model.learn(total_timesteps=np.int(1e9), log_interval=10, callback = callback)
+    
+    #env =  Reverse_variable_trailer_number_environment()
+    
+    # Logs will be saved in log_dir/monitor.csv
+    #env = Monitor(env, log_dir, allow_early_resets=True)
+    
+    #env = SubprocVecEnv( [env])
+    
+    model = PPO2(MlpPolicy, env, verbose=1) 
+    
+    
+    
+    
+    model.learn(total_timesteps=np.int(1e3), log_interval=10, callback = callback)
     model.save("ppo2_trailer")
+    
+def moving_average(values, window):
+    """
+    Smooth values by doing a moving average
+    :param values: (numpy array)
+    :param window: (int)
+    :return: (numpy array)
+    """
+    weights = np.repeat(1.0, window) / window
+    return np.convolve(values, weights, 'valid')
+
+
+def plot_results(log_folder, title='Learning Curve'):
+    """
+    plot the results
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), 'timesteps')
+    y = moving_average(y, window=50)
+    # Truncate x
+    x = x[len(x) - len(y):]
+
+    fig = plt.figure(title)
+    plt.plot(x, y)
+    plt.xlabel('Number of Timesteps')
+    plt.ylabel('Rewards')
+    plt.title(title + " Smoothed")
+    plt.show()
     
 if __name__ == "__main__":
     train()
+    plot_results(log_dir) 
